a1d4d8d779faa8d8ad60bdca48edd3818d110b1d
Pavel Gordeev <pavelgq@yandex.ru>
2026-01-05 22:34:33 +0300

    refactor: update to React 18 createRoot API and fix JSX config

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import './styles/index.css'
 import { Provider } from 'react-redux'
 import { App } from './pages/App'
 import { store } from './store'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error('Failed to find the root element')
+}
+
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
