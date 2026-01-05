90ba1637d30a2fcae76adc59d5a59b08ce7ce1cd
Pavel Gordeev <pavelgq@yandex.ru>
2026-01-05 22:34:28 +0300

    refactor: replace store subscription with Redux middleware for localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, Middleware } from '@reduxjs/toolkit'
 
 import todoReducer from './components/TodoItem/todoSlice'
 import listReducer from './components/List/listSlice'
+
+const localStorageMiddleware: Middleware = (store) => (next) => (action) => {
+  const result = next(action)
+  try {
+    const state = store.getState()
+    localStorage.setItem('store', JSON.stringify(state))
+  } catch (error) {
+    console.error('Ошибка при сохранении состояния в localStorage:', error)
+  }
+  return result
+}
 
 export const store = configureStore({
   reducer: {
     todo: todoReducer,
     list: listReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 })
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
-
-const saveStoreToLocalStorage = () => {
-  const lastStore = store.getState()
-  localStorage.setItem('store', JSON.stringify(lastStore))
-}
-
-store.subscribe(saveStoreToLocalStorage)
