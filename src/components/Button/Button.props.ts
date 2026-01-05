7d9ce60f3e8cea28ca78f841b0e8b772e0625d73
Pavel Gordeev <pavelgq@yandex.ru>
2026-01-05 22:56:43 +0300

    refactor: format component props files

--- src/components/Button/Button.props.ts
+++ src/components/Button/Button.props.ts
@@ -1,10 +1,9 @@
 import React, { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 
-export interface ButtonProps
-  extends DetailedHTMLProps<
-    HTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
+export interface ButtonProps extends DetailedHTMLProps<
+  HTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> {
   children: ReactNode
   appearence: 'primary' | 'ghost' | 'delete'
   arrow?: 'right' | 'down' | 'none'
